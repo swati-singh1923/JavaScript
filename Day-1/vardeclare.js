a1bb0305af9df49a4160487f5ab63f3a91ae24ee
Swati Singh <swatis84ingh@gmail.com>
2024-07-15 06:58:02 +0000

    Conatainer config file

---
+++ Day-1/vardeclare.js
@@ -0,0 +1,18 @@
+/*Task:1 Declare a variable using var, assign it a number, 
+and log the value to the console.*/ 
+
+var number = 1234;
+console.log(number);
+
+/*Task:2 Declare a variable using let, assign it a string, 
+and log the value to the console.*/ 
+
+let str = "Swati Singh";
+console.log(str);
+
+/* Task:3 Declare a variable using const, assign it a boolean value, 
+and log the value to the console.*/ 
+
+const isLogIn = 1;
+const isLoggedIn = Boolean(isLogIn)
+console.log(isLoggedIn);
