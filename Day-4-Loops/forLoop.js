c1c03ba505ad4f6af5a6df83c9da6b10a88aa552
Swati Singh <swatis84ingh@gmail.com>
2024-08-01 07:03:07 +0000

    Adding for loop

---
+++ Day-4-Loops/forLoop.js
@@ -0,0 +1,35 @@
+/* Task 1:Write a program to print numbers from 1 to 10 using a for loop. */
+
+   for(let i=1; i<= 10; i++){
+    console.log(i);
+   }
+
+/* Task 2:Write a program to print multiplication of 5 usin for loop. */
+
+   for(let i=5; i<=50; i=i+5){
+    console.log(i);
+   }
+
+/* Task 3:Write a program to print all odd number from 1 to 20 using for loop. */ 
+
+   for(let i=1; i<=20; i++){
+    if(i%2 == 1){
+        console.log(i);
+    }
+   }
+
+//Backword 
+ 
+   for(let i=20; i>=1; i--){
+    if(i%2 == 1){
+        console.log(i);
+    }
+   }
+
+/* Task 4:Print all the even numbers from 2 to 20. */
+
+   for(let i=1; i<=20; i++){
+    if(i%2 == 0){
+        console.log(i);
+    }
+   }
