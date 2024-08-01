836761bce6a59541f1478644095fb603f30c9536
Swati Singh <swatis84ingh@gmail.com>
2024-08-01 07:02:37 +0000

    Adding for loop and while loop

---
+++ Day-4-Loops/whileLoop.js
@@ -0,0 +1,17 @@
+/* Task 1:Write a program to calculate the sum of numbers from 1 to 10 using while loop. */
+
+   let i = 1;
+   let sum = 0;
+   while(i<=10){
+    sum = sum + i;
+    console.log(sum);
+    i++;
+   }
+
+/* Task 2:Write a program to print numbers from 10 to 1 using while loop. */
+
+   let j = 10;
+   while(j>=1){
+    console.log(j);
+    j--;
+   }
