bee0b0e110e420a21167745dd3bea7467d335352
Swati Singh <swatis84ingh@gmail.com>
2024-08-03 07:42:12 +0000

    Added do-while loop

---
+++ Day-4-Loops/do-while.js
@@ -0,0 +1,12 @@
+/* Task  1:Write a program to print numbers from 1 to 10 using do-while loop. */ 
+
+   let i = 1;
+   do{
+    console.log(i);
+    i++;
+   }
+   while(i<=10);
+
+/* Task 2:Write a program to calculate factorial of a number using do-while loop. */ 
+
+  
