3f63148d1d630c83cb9c648203ab7e78f2aba71f
Swati Singh <swatis84ingh@gmail.com>
2024-08-08 04:59:34 +0000

    Added for of loop

--- Day-6-Array/array_Loops.js
+++ Day-6-Array/array_Loops.js
@@ -31,3 +31,36 @@ for(let i=1; i<fruits.length; i=i+2){
 for(let i=0; i<fruits.length; i=i+2){
     console.log("Even array",i, fruits[i]);
 }
+
+//Loops with Nested Arrays
+
+let heros = [["ironman", "spiderman", "thor","dr. strange"], ["wonder women", "superman", "flash"]];
+for(let i=0; i<heros.length; i++){
+    console.log(`Lists of: ${i+1}`)
+    for(let j=0; j<heros[i].length; j++)
+        console.log("J =",j, heros[i][j]);
+}
+
+//For of loop: Used for access to collections of items.
+
+let colors = ["red", "yellow", "green", "black", "white", "blue"];
+for(let color of colors){
+    console.log(color);
+}
+
+//For of loop for string
+
+let char = "Hello World!"
+for(let chars of char){
+    console.log(chars);
+}
+
+//Nested for of loop
+
+for(list of heros){
+    console.log(list);
+    for(hero of list){
+        console.log(hero);
+    }
+}
+
